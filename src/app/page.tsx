7455d307e0b0146a74f1ce1f1989ec0fc4ddcee1
Jack Herrington <jherr@pobox.com>
2023-08-27 18:23:31 -0700

    Forcing dynamic on the RSC

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { serverClient } from "./_trpc/serverClient";
 
 import TodoList from "./_components/TodoList";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const todos = await serverClient.getTodos();
   return (
